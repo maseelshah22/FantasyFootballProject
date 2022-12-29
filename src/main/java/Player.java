10c5340d8d2c557afe52de37788cdae8cc785d14
Maseel Shah <dda5us@virginia.edu>
2022-12-29 14:51:29 -0500

    creating player object

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,91 @@
 public class Player {
+
+    String name;
+    int number;
+    String position; // QB, WR, TE, RB --> abbreviations only
+    int passingYards;
+    int passingTouchdowns;
+    int rushingYards;
+    int rushingTouchdowns;
+    int recYards;
+    int receptions;
+
+    public Player(){
+
+    }
+
+    public Player(String name, String number, String position){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+
+    public int getPassingYards() {
+        return passingYards;
+    }
+
+    public void setPassingYards(int passingYards) {
+        this.passingYards = passingYards;
+    }
+
+    public int getPassingTouchdowns() {
+        return passingTouchdowns;
+    }
+
+    public void setPassingTouchdowns(int passingTouchdowns) {
+        this.passingTouchdowns = passingTouchdowns;
+    }
+
+    public int getRushingYards() {
+        return rushingYards;
+    }
+
+    public void setRushingYards(int rushingYards) {
+        this.rushingYards = rushingYards;
+    }
+
+    public int getRushingTouchdowns() {
+        return rushingTouchdowns;
+    }
+
+    public void setRushingTouchdowns(int rushingTouchdowns) {
+        this.rushingTouchdowns = rushingTouchdowns;
+    }
+
+    public int getRecYards() {
+        return recYards;
+    }
+
+    public void setRecYards(int recYards) {
+        this.recYards = recYards;
+    }
+    public int getReceptions() {
+        return receptions;
+    }
+    public void setReceptions(int receptions) {
+        this.receptions = receptions;
+    }
 }
